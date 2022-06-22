8322d71916b74ba9d5bbe98ee450cb42aa557980
Daniel Aranza <daniaranzagarcia@gmail.com>
2022-06-22 15:46:25 -0500

    ✨ add: calculator helper

--- src/views/Calculator/ResultSection.tsx
+++ src/views/Calculator/ResultSection.tsx
@@ -4,59 +4,66 @@ import ResultRecommendation from './ResultRecommendation';
 
 import { products } from '../../@fake/productsFake';
 import { useState } from 'react';
+import { PetFeedData } from '../../helpers/calculator';
 
-const ResultSection = () => {
+const ResultSection = ({ data }: ResultSectionProps) => {
   const [selected, setSelected] = useState(0);
   return (
     <div className='w-full rounded-t-3xl bg-sixth -mt-5 z-10 animate__animated animate__fadeIn md:px-20 xl1:px-[12.8rem] xl2:px-[17.8rem]'>
       <div className='mb-8 mt-14 text-center px-9 md:px-0'>
-        <span className='text-primary text-xl font-bold lg:text-[24px]'>¡PIXEL debe consumir un total de 670 gramos al día en 2 porciones!</span>
+        <span className='text-primary text-xl font-bold lg:text-[24px]'>{data?.msg}</span>
       </div>
       <span className='ml-3 px-3 md:px-0'>Recomendados:</span>
 
       {/* Carrousel */}
-      <Carrousel className='pt-10 pb-10 lg:px-[9rem]' onSlideChange={setSelected} navigation breakpoints={{
-        100: {
-          slidesPerView: 1,
-          spaceBetween: 0,
-          initialSlide: 2,
-        },
-        300: {
-          slidesPerView: 2,
-          spaceBetween: 0,
-          initialSlide: 2,
-        },
-        500: {
-          slidesPerView: 2,
-          spaceBetween: 0,
-          initialSlide: 2,
-        },
+      <Carrousel
+        className='pt-10 pb-10 lg:px-[9rem]'
+        onSlideChange={setSelected}
+        navigation
+        breakpoints={{
+          100: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+            initialSlide: 2,
+          },
+          300: {
+            slidesPerView: 2,
+            spaceBetween: 0,
+            initialSlide: 2,
+          },
+          500: {
+            slidesPerView: 2,
+            spaceBetween: 0,
+            initialSlide: 2,
+          },
 
-        1536: {
-          slidesPerView: 2.5,
-          spaceBetween: 0,
-          initialSlide: 2,
-        },
+          1536: {
+            slidesPerView: 2.5,
+            spaceBetween: 0,
+            initialSlide: 2,
+          },
 
-        1800: {
-          slidesPerView: 3,
-          spaceBetween: 0,
-          initialSlide: 2,
-        },
-
-      }}>
-        {
-          products.map((card, i) =>
-            <ProductCard
-              selected={selected === i}
-              key={`calculatorCart-${card.id}`}
-              product={card}
-              showControls={false} />
-          )
-        }
+          1800: {
+            slidesPerView: 3,
+            spaceBetween: 0,
+            initialSlide: 2,
+          },
+        }}
+      >
+        {products.map((card, i) => (
+          <ProductCard
+            selected={selected === i}
+            key={`calculatorCart-${card.id}`}
+            product={card}
+            showControls={false}
+          />
+        ))}
       </Carrousel>
       <div className='text-center mt-8 mb-5 font-subTitles font-bold'>
-        <span>Dieta recomendada para 4 semanas = 30 rollitos, <span className='text-primary'>¡combínalas como tu quieras!</span></span>
+        <span>
+          Dieta recomendada para 4 semanas = 30 rollitos,{' '}
+          <span className='text-primary'>¡combínalas como tu quieras!</span>
+        </span>
       </div>
       <ResultRecommendation />
       {/* <img src={backgroundCalculator} className='absolute -z-10' /> */}
@@ -64,5 +71,8 @@ const ResultSection = () => {
   );
 };
 
-export default ResultSection;
+interface ResultSectionProps {
+  data?: PetFeedData;
+}
 
+export default ResultSection;
--- src/views/Calculator/calculatorSection.tsx
+++ src/views/Calculator/calculatorSection.tsx
@@ -8,6 +8,7 @@ import { SelectItem } from '../../components/form/selectField';
 import { useForm } from '../../hooks';
 
 import { dogCalculator, fullDogCalculator, backgroundCalculator } from '../../assets/images';
+import { getPetFeedData, PetFeedData, PetInfo } from '../../helpers/calculator';
 
 const ages: SelectItem[] = [
   { value: '6', label: '6 Meses' },
@@ -26,55 +27,75 @@ const exercises: SelectItem[] = [
   { value: 'high', label: 'Alta' },
 ];
 
-export const CalculatorSection = ({ setView }:CalculatorSectionProps) => {
+export const CalculatorSection = ({ setView }: CalculatorSectionProps) => {
   // Hooks
-  const { form, onSubmit, handleFormChange, handleSelectChange } = useForm<CalculatorFormType>({
-    name: '',
-    type: types[0],
-    typeOptions: types,
-    age: ages[0],
-    ageOptions: ages,
-    exactAge: 0,
-    allergies: '',
-    diseases: '',
-    exercise: exercises[0],
-    exerciseOptions: exercises,
-    idealWeight: 0
-  }, form => handleSubmit(form));
+  const { form, onSubmit, handleFormChange, handleSelectChange } = useForm<CalculatorFormType>(
+    {
+      name: '',
+      type: types[0],
+      typeOptions: types,
+      age: ages[0],
+      ageOptions: ages,
+      exactAge: 0,
+      allergies: '',
+      diseases: '',
+      exercise: exercises[0],
+      exerciseOptions: exercises,
+      idealWeight: 0,
+    },
+    form => handleSubmit(form)
+  );
+
   const [isResult, setIsResult] = useState(false);
+  const [feedData, setFeedData] = useState<PetFeedData>();
 
   // Methods
   const handleSubmit = (form: CalculatorFormType) => {
+    const data = getPetFeedData({
+      activity: form.exercise.value as PetInfo['activity'],
+      age: form.exactAge,
+      range: 'senior',
+      type: 'dog',
+      name: form.name,
+      weight: form.idealWeight,
+    });
+    setFeedData(data);
+
     setIsResult(true);
   };
 
   // Component
   return (
     <>
-      {isResult ? (<ResultSection />)
-        :
-        (
-          <div className='w-full flex flex-col gap-2 rounded-t-3xl bg-sixth -mt-5 z-10'>
-            <div className='flex gap-2'>
-              {/* Background */}
-              <div className='hidden tall:hidden md:w-1/2 md:overflow-hidden md:block'>
-                <img src={backgroundCalculator} className='md:absolute md:-z-10' />
-                <img src={dogCalculator} className='w-full z-10' />
-              </div>
-              {/* Form */}
-              <CalculatorForm form={form} onSubmit={onSubmit} onChange={handleFormChange} onSelectChange={handleSelectChange} setView={setView}/>
+      {isResult ? (
+        <ResultSection data={feedData} />
+      ) : (
+        <div className='w-full flex flex-col gap-2 rounded-t-3xl bg-sixth -mt-5 z-10'>
+          <div className='flex gap-2'>
+            {/* Background */}
+            <div className='hidden tall:hidden md:w-1/2 md:overflow-hidden md:block'>
+              <img src={backgroundCalculator} className='md:absolute md:-z-10' />
+              <img src={dogCalculator} className='w-full z-10' />
             </div>
-            {/* Mobile Dog */}
-            <img src={fullDogCalculator} className='block w-full z-10 pb-5 sm:hidden' />
+            {/* Form */}
+            <CalculatorForm
+              form={form}
+              onSubmit={onSubmit}
+              onChange={handleFormChange}
+              onSelectChange={handleSelectChange}
+              setView={setView}
+            />
           </div>
-        )
-      }
+          {/* Mobile Dog */}
+          <img src={fullDogCalculator} className='block w-full z-10 pb-5 sm:hidden' />
+        </div>
+      )}
     </>
   );
 };
 
 interface CalculatorSectionProps {
-  setView: React.Dispatch<React.SetStateAction<number>>
+  setView: React.Dispatch<React.SetStateAction<number>>;
 }
 
 export default CalculatorSection;
